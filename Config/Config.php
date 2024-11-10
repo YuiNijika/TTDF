87a31c15d6b793dbfa715527874bba58722b2c68
ShuShuicu <Tomori@miomoe.cn>
2024-11-10 21:10:03 +0800

    Cravatar

--- Config/Config.php
+++ Config/Config.php
@@ -4,7 +4,7 @@
  * 使用说明 / 开发文档请查看README.md；
  * 如有任何不懂的问题欢迎联系作者<a href="https://space.bilibili.com/435502585"> · B站 · </a>提供帮助。
  * @author 鼠子Tomoriゞ
- * @version 1.0.4
+ * @version 1.0.5
  * @link https://blog.miomoe.cn/
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
@@ -12,3 +12,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 require_once 'Get.php';
 // 主题设置功能
 require_once 'Options.php';
+// 配置默认Cravatar
+$avatarCdn = 'https://cravatar.cn/avatar/';
+// 定义常量
+define('__TYPECHO_GRAVATAR_PREFIX__', $avatarCdn);
