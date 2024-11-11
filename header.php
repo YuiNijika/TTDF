1ed4bf8381c946cbcfc460cbabefcc7d8fadbf65
鼠子 <Tomori.miomoe.cn>
2024-11-12 02:37:10 +0800

    1.0.8

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ if (!defined("__TYPECHO_ROOT_DIR__")) {
         <meta name="renderer" content="webkit"/>
         <link href="<?php echo Get::Options("faviconUrl") ? Get::Options("faviconUrl") : Get::AssetsUrl() . "/images/favicon.svg"; ?>" rel="icon" />
         <link rel="stylesheet" href="<?php Get::AssetsUrl(); ?>/style.css?ver=<?php GetTheme::Ver(); ?>">
-        <title><?php $archiveTitle = $this->archiveTitle(
+        <title><?php $archiveTitle = GetPost::ArchiveTitle(
             [
                 "category" => _t("「%s」分类"),
                 "search" => _t("搜索结果"),
@@ -22,12 +22,12 @@ if (!defined("__TYPECHO_ROOT_DIR__")) {
             ],""," - "
         );
         echo $archiveTitle;
-        if ($this->_currentPage > 1) {
+        if (Get::Is("index") && !empty(Get::Options("subTitle"))) {
             echo "「第" . $this->_currentPage . "页」 - ";
         }
         $title = Get::Options("title");
         echo $title;
-        if ($this->is("index") && !empty(Get::Options("subTitle"))) {
+        if (Get::Is("index") && !empty(Get::Options("subTitle"))) {
             echo " - ";
             $subTitle = Get::Options("subTitle");
             echo $subTitle;
