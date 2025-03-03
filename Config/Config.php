1a4185f5da7c72a692c103455440d31786d508be
Tomoriゞ <165134730+ShuShuicu@users.noreply.github.com>
2025-03-03 21:38:09 +0800

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -4,7 +4,7 @@
  * 使用说明 / 开发文档请查看README.md；
  * 如有任何不懂的问题欢迎联系作者<a href="https://space.bilibili.com/435502585"> · B站 · </a>提供帮助。
  * @author 鼠子(Tomoriゞ)、Sualiu
- * @version 1.1.4
+ * @version 1.1.5
  * @link https://blog.miomoe.cn/
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
