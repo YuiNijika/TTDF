40b97f177c22ff48df475cc5a57233bcc9dc6b9e
Tomoriゞ <Tomori@miomoe.cn>
2025-03-15 02:31:21 +0800

    updata

--- Core/TTDF/Functions/ClassGetPost.php
+++ Core/TTDF/Functions/ClassGetPost.php
@@ -15,151 +15,293 @@ class GetPost extends Typecho_Widget {
     public function __wakeup() {}
 
     // 获取标题
-    public static function Title()
+    public static function Title($echo = true)
     {
         try {
-            echo self::getArchive()->title;
+            $title = self::getArchive()->title;
+            if ($echo) {
+                echo $title;
+            } else {
+                return $title;
+            }
         } catch (Exception $e) {
             self::handleError('获取标题失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取日期
-    public static function Date($format = 'Y-m-d')
+    public static function Date($format = 'Y-m-d', $echo = true)
     {
         try {
-            return self::getArchive()->date($format);
+            $date = self::getArchive()->date($format);
+            if ($echo) {
+                echo $date;
+            } else {
+                return $date;
+            }
         } catch (Exception $e) {
-            return self::handleError('获取日期失败', $e, '');
+            $result = self::handleError('获取日期失败', $e, '');
+            if ($echo) {
+                echo $result;
+            } else {
+                return $result;
+            }
         }
     }
 
     // 获取分类
-    public static function Category($split = ',', $link = true, $default = '暂无分类')
+    public static function Category($split = ',', $link = true, $default = '暂无分类', $echo = true)
     {
         try {
-            echo self::getArchive()->category($split, $link, $default);
+            $category = self::getArchive()->category($split, $link, $default);
+            if ($echo) {
+                echo $category;
+            } else {
+                return $category;
+            }
         } catch (Exception $e) {
             self::handleError('获取分类失败', $e);
-            echo $default;
+            if ($echo) {
+                echo $default;
+            } else {
+                return $default;
+            }
         }
     }
 
     // 获取标签
-    public static function Tags($split = ',', $link = true, $default = '暂无标签')
+    public static function Tags($split = ',', $link = true, $default = '暂无标签', $echo = true)
     {
         try {
-            echo self::getArchive()->tags($split, $link, $default);
+            $tags = self::getArchive()->tags($split, $link, $default);
+            if ($echo) {
+                echo $tags;
+            } else {
+                return $tags;
+            }
         } catch (Exception $e) {
             self::handleError('获取标签失败', $e);
-            echo $default;
+            if ($echo) {
+                echo $default;
+            } else {
+                return $default;
+            }
         }
     }
 
     // 获取摘要
-    public static function Excerpt($length = 0)
+    public static function Excerpt($length = 0, $echo = true)
     {
         try {
             $excerpt = strip_tags(self::getArchive()->excerpt);
             if ($length > 0) {
                 $excerpt = mb_substr($excerpt, 0, $length, 'UTF-8');
             }
-            echo $excerpt;
+            if ($echo) {
+                echo $excerpt;
+            } else {
+                return $excerpt;
+            }
         } catch (Exception $e) {
             self::handleError('获取摘要失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取永久链接
-    public static function Permalink()
+    public static function Permalink($echo = true)
     {
         try {
-            echo self::getArchive()->permalink;
+            $permalink = self::getArchive()->permalink;
+            if ($echo) {
+                echo $permalink;
+            } else {
+                return $permalink;
+            }
         } catch (Exception $e) {
             self::handleError('获取永久链接失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取内容
-    public static function Content()
+    public static function Content($echo = true)
     {
         try {
-            echo self::getArchive()->content;
+            $content = self::getArchive()->content;
+            if ($echo) {
+                echo $content;
+            } else {
+                return $content;
+            }
         } catch (Exception $e) {
             self::handleError('获取内容失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取文章数
-    public static function PostsNum()
+    public static function PostsNum($echo = true)
     {
         try {
-            echo self::getArchive()->postsNum;
+            $postsNum = self::getArchive()->postsNum;
+            if ($echo) {
+                echo $postsNum;
+            } else {
+                return $postsNum;
+            }
         } catch (Exception $e) {
             self::handleError('获取文章数失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取页面数
-    public static function PagesNum()
+    public static function PagesNum($echo = true)
     {
         try {
-            echo self::getArchive()->pagesNum;
+            $pagesNum = self::getArchive()->pagesNum;
+            if ($echo) {
+                echo $pagesNum;
+            } else {
+                return $pagesNum;
+            }
         } catch (Exception $e) {
             self::handleError('获取页面数失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取标题
-    public static function ArchiveTitle($format = '', $default = '', $connector = '')
+    public static function ArchiveTitle($format = '', $default = '', $connector = '', $echo = true)
     {
         try {
             if (empty($format)) {
-                echo self::getArchive()->archiveTitle;
+                $archiveTitle = self::getArchive()->archiveTitle;
             } else {
-                echo self::getArchive()->archiveTitle($format, $default, $connector);
+                $archiveTitle = self::getArchive()->archiveTitle($format, $default, $connector);
+            }
+            if ($echo) {
+                echo $archiveTitle;
+            } else {
+                return $archiveTitle;
             }
         } catch (Exception $e) {
             self::handleError('获取标题失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取作者
-    public static function Author()
+    public static function Author($echo = true)
     {
         try {
-            echo self::getArchive()->author->screenName;
+            $author = self::getArchive()->author->screenName;
+            if ($echo) {
+                echo $author;
+            } else {
+                return $author;
+            }
         } catch (Exception $e) {
             self::handleError('获取作者失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取作者头像
-    public static function AuthorAvatar($size = 128)
+    public static function AuthorAvatar($size = 128, $echo = true)
     {
         try {
-            echo self::getArchive()->author->gravatar($size);
+            $avatar = self::getArchive()->author->gravatar($size);
+            if ($echo) {
+                echo $avatar;
+            } else {
+                return $avatar;
+            }
         } catch (Exception $e) {
             self::handleError('获取作者头像失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取作者链接
-    public static function AuthorPermalink()
+    public static function AuthorPermalink($echo = true)
     {
         try {
-            echo self::getArchive()->author->permalink;
+            $authorPermalink = self::getArchive()->author->permalink;
+            if ($echo) {
+                echo $authorPermalink;
+            } else {
+                return $authorPermalink;
+            }
         } catch (Exception $e) {
             self::handleError('获取作者链接失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
         }
     }
 
     // 获取文章字数
-    public static function WordCount() {
-        $cid = self::getArchive()->cid;
-        $db = DB::getInstance();
-        $text = $db->getArticleText($cid);
-        $text = preg_replace("/[^\x{4e00}-\x{9fa5}]/u", "", $text);
-        echo mb_strlen($text, 'UTF-8');
+    public static function WordCount($echo = true)
+    {
+        try {
+            $cid = self::getArchive()->cid;
+            $db = DB::getInstance();
+            $text = $db->getArticleText($cid);
+            $text = preg_replace("/[^\x{4e00}-\x{9fa5}]/u", "", $text);
+            $wordCount = mb_strlen($text, 'UTF-8');
+            if ($echo) {
+                echo $wordCount;
+            } else {
+                return $wordCount;
+            }
+        } catch (Exception $e) {
+            self::handleError('获取文章字数失败', $e);
+            if ($echo) {
+                echo '';
+            } else {
+                return '';
+            }
+        }
     }
 }
