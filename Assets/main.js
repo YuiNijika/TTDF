d3b1912b7ccb6cde3d8fe380a1ed51d243a4aa9b
ShuShuicu <Tomori@miomoe.cn>
2025-04-21 16:16:12 +0800

    移除打印logo

--- Assets/main.js
+++ Assets/main.js
@@ -1,5 +1,6 @@
-//** 2025 Typecho Theme Development Framework ©️ Copyright By Tomoriゞ */
-const printLogo = () => {
-    console.log("\n %c 基于TTDF开发框架 %c https://github.com/ShuShuicu/Typecho-Theme-Development-Framework \n", "color: #fadfa3; background: #030307; padding:3px; font-size:12px;", "color: #2abd4e;background: #16171a; padding: 3px; font-size: 12px;");
-};
-printLogo();
+/* 
+    Typecho Theme Development Framework
+    TTDF · 一个面向小白简单易懂的Typecho主题模板开发框架
+    @Author 鼠子Tomoriゞ
+    https://github.com/ShuShuicu/Typecho-Theme-Development-Framework
+*/
