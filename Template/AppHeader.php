21d601dc8c8e7496b2070bbd3c122018672caef1
ShuShuicu <Tomori@miomoe.cn>
2025-04-22 16:31:24 +0800

    lang

--- Template/AppHeader.php
+++ Template/AppHeader.php
@@ -2,7 +2,7 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 <!doctype html>
-<html lang="zh-CN">
+<html lang="<?php echo Get::Options('lang', false) ? Get::Options('lang', false) : 'zh-CN' ?>">
 
 <head>
     <?php TTDF_Hook::do_action('load_head'); ?>
