8e8fb7a23c108d6e10bef24cd841a2c5317fa2cd
ShuShuicu <Tomori@miomoe.cn>
2025-05-01 00:33:03 +0800

    优化SEO

--- Core/TTDF/Functions/ClassTTDF.php
+++ Core/TTDF/Functions/ClassTTDF.php
@@ -91,11 +91,11 @@ class TTDF
     public static function HeadMetaOG()
     {
     ?>
-    <meta name="og:description" content="<?php TTDF_SEO_Description(); ?>" />
+    <meta name="og:description" content="<?php echo TTDF_SEO_Helper::getDescription(); ?>" />
     <meta property="og:locale" content="<?php echo Get::Options('lang') ? Get::Options('lang') : 'zh-CN' ?>" />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="<?php Get::Options('title', true) ?>" />
-    <meta property="og:title" content="<?php TTDF_SEO_Title(); ?>" />
+    <meta property="og:title" content="<?php echo TTDF_SEO_Helper::getPageTitle(); ?>" />
     <meta property="og:url" content="<?php Get::PageUrl(); ?>" />
     <link rel="canonical" href="<?php Get::PageUrl(true, false, null, true); ?>" />
 <?php
