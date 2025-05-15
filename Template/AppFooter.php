f07bfd9bcbb95ee0ab8beae673da8b229fa7faff
ShuShuicu <Tomori@miomoe.cn>
2025-05-16 06:29:34 +0800

    Update AppFooter.php

--- Template/AppFooter.php
+++ Template/AppFooter.php
@@ -3,7 +3,6 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 
     </main>
-    <script src="<?php GetTheme::AssetsUrl() ?>/main.js?ver=<?php GetTheme::Ver(); ?>"></script>
     <?php TTDF_Hook::do_action('load_foot'); ?>
 </body>
 
