08cd715627436b4238ca1e9c484e024654755e9f
ShuShuicu <Tomori@miomoe.cn>
2025-05-18 22:38:13 +0800

    Update TTDF.php

--- Core/TTDF/Widget/TTDF.php
+++ Core/TTDF/Widget/TTDF.php
@@ -4,6 +4,30 @@
  * TTDF Class
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * 加载时间
+ * @return bool
+ */
+function TTDF_TimerStart()
+{
+    global $timestart;
+    $mtime     = explode(' ', microtime());
+    $timestart = $mtime[1] + $mtime[0];
+    return true;
+}
+TTDF_TimerStart();
+function TTDF_TimerStop($display = 0, $precision = 3)
+{
+    global $timestart, $timeend;
+    $mtime     = explode(' ', microtime());
+    $timeend   = $mtime[1] + $mtime[0];
+    $timetotal = number_format($timeend - $timestart, $precision);
+    $r         = $timetotal < 1 ? $timetotal * 1000 . " ms" : $timetotal . " s";
+    if ($display) {
+        echo $r;
+    }
+    return $r;
+}
 class TTDF
 {
 
@@ -142,30 +166,6 @@ class TTDF_Widget
 
     public static function TimerStop(?bool $echo = true)
     {
-        /**
-         * 加载时间
-         * @return bool
-         */
-        function TTDF_TimerStart()
-        {
-            global $timestart;
-            $mtime     = explode(' ', microtime());
-            $timestart = $mtime[1] + $mtime[0];
-            return true;
-        }
-        TTDF_TimerStart();
-        function TTDF_TimerStop($display = 0, $precision = 3)
-        {
-            global $timestart, $timeend;
-            $mtime     = explode(' ', microtime());
-            $timeend   = $mtime[1] + $mtime[0];
-            $timetotal = number_format($timeend - $timestart, $precision);
-            $r         = $timetotal < 1 ? $timetotal * 1000 . " ms" : $timetotal . " s";
-            if ($display) {
-                echo $r;
-            }
-            return $r;
-        }
         try {
             if ($echo) echo TTDF_TimerStop();
             ob_start();  // 开启输出缓冲
