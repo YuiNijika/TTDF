60a71e1f0b53fbcaea7974a64346f176118f4735
ShuShuicu <Tomori@miomoe.cn>
2025-06-12 13:51:32 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
-
 /**
- * 主题配置文件
+ * 感谢选择`TTDF`进行开发
+ * 如需自定义代码请在`/Core/Code.php`中编写
+ * 框架的基础配置在`/Core/TTDF.Config.php`中修改
+ * 如果觉得还不错的话请给`TTDF`点个 *Star* 谢谢!
+ * @link https://github.com/ShuShuicu/TTDF
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
@@ -10,7 +13,7 @@ class Main
     public static function init()
     {
         // 引入框架配置文件
-        require_once 'Core/TTDF.Config.php';
+        require_once __DIR__ . '/Core/TTDF.Config.php';
         // 引入主题自定义代码
         if(file_exists(__DIR__ . '/Core/Code.php')) {
             // 先在`Core`目录下创建`Code.php`文件
