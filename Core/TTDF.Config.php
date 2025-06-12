e44ba3d221774d9ae7a62e1b1acbecdcc0b9b4cc
ShuShuicu <Tomori@miomoe.cn>
2025-06-12 10:02:50 +0800

    Update TTDF.Config.php

--- Core/TTDF.Config.php
+++ Core/TTDF.Config.php
@@ -19,11 +19,5 @@ $defineTTDFConfig = [
     ],
 ];
 
-// 配置默认头像源
-define('__TYPECHO_GRAVATAR_PREFIX__', $defineTTDFConfig['Modules']['GravatarPrefix'] ?? 'https://cravatar.cn/avatar/');
-// 主题注册设置项为 * TTDF_RESTAPI_Switch * 则代理启用REST API
-define('__TTDF_RESTAPI__', $defineTTDFConfig['Modules']['RestApi'] ?? false);
-define('__TTDF_RESTAPI_ROUTE__', $defineTTDFConfig['Modules']['RestApiRoute'] ?? 'ty-json');
-
 // 加载核心文件
 require_once 'TTDF/Main.php';
