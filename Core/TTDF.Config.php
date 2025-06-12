c48d84dd8ba4eb73f4ba92f1c5d99283b55c8e0b
ShuShuicu <Tomori@miomoe.cn>
2025-06-12 09:51:29 +0800

    Update TTDF.Config.php

--- Core/TTDF.Config.php
+++ Core/TTDF.Config.php
@@ -6,15 +6,19 @@
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
-// 是否开启自定义字段
-define('__TTDF_FIELDS__', false); 
+$TTDFConfig = [
+    'Fields' => false, // 是否开启自定义字段
+    'TyAjax' => false, // 是否启用TyAjax扩展
+    'GravatarPrefix' => 'https://cravatar.cn/avatar/', // 配置默认头像源
+    'RestApi' => false, // 设置 REST API 状态
+    'RestApiRoute' => 'ty-json', // 设置 REST API 路由
+];
+
 // 配置默认头像源
-define('__TYPECHO_GRAVATAR_PREFIX__', 'https://cravatar.cn/avatar/');
-// 设置 REST API 状态
-// *主题注册设置项为 TTDF_RESTAPI_Switch * 则代理启用
-define('__TTDF_RESTAPI__', false);
-// 设置 REST API 路由
-define('__TTDF_RESTAPI_ROUTE__', 'API');
+define('__TYPECHO_GRAVATAR_PREFIX__', $TTDFConfig['GravatarPrefix'] ?? 'https://cravatar.cn/avatar/');
+// 主题注册设置项为 * TTDF_RESTAPI_Switch * 则代理启用REST API
+define('__TTDF_RESTAPI__', $TTDFConfig['RestApi'] ?? false);
+define('__TTDF_RESTAPI_ROUTE__', $TTDFConfig['RestApiRoute'] ?? 'ty-json');
 
 // 加载核心文件
 require_once 'TTDF/Main.php';
