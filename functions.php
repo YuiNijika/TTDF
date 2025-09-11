5946d9c47dfea3b9393e61447f277608738e0942
鼠子 <Tomori@miomoe.cn>
2025-09-12 03:04:09 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,10 +8,11 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 // 加载框架核心文件
-if (file_exists(__DIR__ . '/core/Main.php')) {
-    require_once __DIR__ . '/core/Main.php';
+$ttdfFrameworkPath = __DIR__ . '/core/Main.php';
+if (file_exists($ttdfFrameworkPath)) {
+    require $ttdfFrameworkPath;
 } else {
-    throw new Exception('TTDF核心文件加载失败, 请检查文件是否存在: ' . __DIR__ . '/core/Main.php');
+    throw new Exception('TTDF核心文件加载失败, 请检查框架是否安装完整: ' . $ttdfFrameworkPath);
 }
 
 /**
